78be2620763181e179bd02684ca7a67bf06c0bda
Priyanshul. <piyanshul1307@gmail.com>
2024-09-11 00:20:40 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -8,26 +8,26 @@ def play():
    
     with open("words.txt","r") as f:
         words=f.readlines()
-    #select a random word from list and covert to upper case
-    index=random.randint(0,len(words)-1)
-    theWord=words[index].strip().upper() #word to be guessed
 
-    totalChance=7 #total chance
+    index=random.randint(0,len(words)-1)
+    theWord=words[index].strip().upper() 
+
+    totalChance=7 
     currentChance=0
     hangman="HANGMAN"
-    guessedWord='_'*len(theWord) #word to be guessed
+    guessedWord='_'*len(theWord)
     ast.speak("WELCOME TO HANGMAN")
-    while totalChance!=0: #repeat till all the chances are exhausted
+    while totalChance!=0:
         print(guessedWord)
-        #get the input from user and convert to upper case since our word is in capitals
+        
         ast.speak("Guess the letter :")
         letter=ast.getAudio().upper() 
-        if letter in theWord: #check if letter is present in word
-            for index in range(len(theWord)): #get all the index of word
-                if theWord[index]==letter: #if letter at index is the letter
-                    #replace the blank line at that index with letter
+        if letter in theWord:
+            for index in range(len(theWord)):
+                if theWord[index]==letter: 
+                    
                     guessedWord=guessedWord[:index]+letter+guessedWord[index+1:]
-            #check if user has guessed correct
+         
             if guessedWord==theWord:
                 ast.speak("Congratulations!!!You Won with ",totalChance," chances to spare")
                 break
